b680e0cf044eb26be1b2ba9792baecbe00cb054c
akoslowski <2005824+akoslowski@users.noreply.github.com>
2023-01-13 09:48:19 +0100

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Andreas Koslowski on 13.01.23.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
